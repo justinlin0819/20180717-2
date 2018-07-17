863bc6e01bb7cbec8e3782f6e75d95d49db4bedd
Your Name <you@example.com>
2018-07-17 14:31:09 +0800

    Initial Commit

---
+++ 20180717-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20180717-2
+//
+//  Created by Justin Lin on 2018/7/17.
+//  Copyright © 2018年 Justin Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
